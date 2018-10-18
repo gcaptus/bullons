a59570fdb3dbd52345dad5bc23115f7408304922
Loïc <loic.dott@gmail.com>
2018-10-18 10:03:06 +0200

    modifications on footer and other part

--- bulles/src/AppBundle/Controller/IndexController.php
+++ bulles/src/AppBundle/Controller/IndexController.php
@@ -6,16 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class IndexController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/index", name="realindex")
      */
-    public function warningAction(Request $request)
+    public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/warning.html.twig', [
+        return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
-    }   
+    }
+
+    
 }
